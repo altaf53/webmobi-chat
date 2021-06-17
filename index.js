2591a2067734afa94cf8ec467ea132a291355da9
altaf53 <aaltafali@gmail.com>
2021-06-17 21:25:45 +0530

    done

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// for parsing application/json
+app.use(express.json()); 
+
+// for parsing application/xwww-
+app.use(express.urlencoded({ extended: true })); 
+
+//GET for index
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+//GET for users
+app.get('/users', (req, res) => {
+    res.sendFile(__dirname + '/users.html');
+})
+
+//GET for chat
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+})
+
+//POST for create-user
+app.post('/create-user', (req, res) => {
+    // var data = qs.parse(data)
+    console.log("User cretaed: " + req.body.username)
+    res.redirect('/chat?id=' + req.body.username);
+})
+
+io.on('connection', (socket) => {
+    // console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+        // console.log('message: ' + msg);
+      });
+
+    socket.on('disconnect', () => {
+    //   console.log('user disconnected');
+    });
+  }); 
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
